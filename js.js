55a50b9bc48f9305a8aa2c405a495cc1f6cff8e3
AshiyroMisachi <riallot.alexandre@outlook.fr>
2022-11-23 19:25:52 +0100

    Mise en avant Attaque
    Mise en avant des personnages et attaque terminé

--- js.js
+++ js.js
@@ -2,27 +2,52 @@ attaqueperso = document.getElementById("attaque");
 pvperso1=document.getElementById("vieperso1")
 pvmonstre1=document.getElementById("viemonstre1");
 texte=document.getElementById("texte");
+
+//recuperation id image perso
 imageperso1=document.getElementById("imageperso1");
 imageperso2=document.getElementById("imageperso2");
+imageperso3=document.getElementById("imageperso3");
+imageperso4=document.getElementById("imageperso4");
+
 let compteur = 1;
 
-
+//Fonction de mise en avant des personnages
 function positionnement() {
     if (compteur == 1) {
         imageperso1.style.left = 20;
+        imageperso2.style.left = 0;
+        imageperso3.style.left = 0;
+        imageperso4.style.left = 0;
     }
     else  if (compteur == 2) {
         imageperso1.style.left = 0;
         imageperso2.style.left = 20;
+        imageperso3.style.left = 0;
+        imageperso4.style.left = 0;
     }
     else if (compteur == 3){
         imageperso1.style.left = 0;
         imageperso2.style.left = 0;
+        imageperso3.style.left = 20;
+        imageperso4.style.left = 0;
+    }
+    else if (compteur == 4) {
+        imageperso1.style.left = 0;
+        imageperso2.style.left = 0;
+        imageperso3.style.left = 0;
+        imageperso4.style.left = 20;
+    }
+    else {
+        imageperso1.style.left = 0;
+        imageperso2.style.left = 0;
+        imageperso3.style.left = 0;
+        imageperso4.style.left = 0;
     }
 }
-
 positionnement()
 
+
+//Systeme attaque joueur 
 attaqueperso.onclick = function() {
     if (compteur == 1){
         compteur += 1;
@@ -44,6 +69,28 @@ attaqueperso.onclick = function() {
             imageperso2.style.left = 20;
         } 
         setTimeout(reposition2,200);
+        setTimeout(positionnement,1000);
+    }
+    else if (compteur == 3) {
+        compteur += 1;
+        pvmonstre1.innerHTML = pvmonstre1.innerHTML-10;
+        texte.innerHTML = "Perso 3 attaque Monstre 1 et inflige 10 de degats !";
+        imageperso3.style.left = 30;
+        function reposition3() {
+            imageperso3.style.left = 20;
+        } 
+        setTimeout(reposition3,200);
+        setTimeout(positionnement,1000);
+    }
+    else if (compteur == 4) {
+        compteur += 1;
+        pvmonstre1.innerHTML = pvmonstre1.innerHTML-10;
+        texte.innerHTML = "Perso 4 attaque Monstre 1 et inflige 10 de degats !";
+        imageperso4.style.left = 30;
+        function reposition4() {
+            imageperso4.style.left = 20;
+        } 
+        setTimeout(reposition4,200);
         setTimeout(positionnement,1000);
     }
 }
