1e69836c865231c8cbacad55fa4175fb9fa58590
AshiyroMisachi <riallot.alexandre@outlook.fr>
2022-11-23 15:17:03 +0100

    HTML JS
    Ajout attaque et contre attaque perso et monstre 1

--- js.js
+++ js.js
@@ -1,8 +1,14 @@
-attaque=document. getElementById("attaque");
-pvmonstre1=document. getElementById("viemonstre1");
-texte=document. getElementById("texte");
+attaqueperso = document.getElementById("attaque");
+pvperso1=document.getElementById("vieperso1")
+pvmonstre1=document.getElementById("viemonstre1");
+texte=document.getElementById("texte");
 
-attaque. onclick=fonction() {
-    pvmonstre1. innerHTML=pvmonstre1. innertHTML-10;
-    afficheAction. innerHTML="Perso 1 attaque Monstre 1 et inflige 10 de dégâts !";
+attaqueperso.onclick=function() {
+    pvmonstre1.innerHTML = pvmonstre1.innerHTML-10;
+    texte.innerHTML = "Perso 1 attaque Monstre 1 et inflige 10 de degats !" ;
+    function contreattaque() {
+        pvperso1.innerHTML = pvperso1.innerHTML-15;
+        texte.innerHTML = "Monstre 1 contre attaque et inflige 15 de degats !";
+    }
+    setTimeout(contreattaque,2000);
 }
