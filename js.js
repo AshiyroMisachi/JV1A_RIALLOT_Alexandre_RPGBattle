d2a2b89b0be10e2ed48413f257820573533468da
AshiyroMisachi <riallot.alexandre@outlook.fr>
2022-11-23 16:38:23 +0100

    Important: Compteur de tour pensé
    On va tenté un truc tkt ça passe

--- js.js
+++ js.js
@@ -3,18 +3,23 @@ pvperso1=document.getElementById("vieperso1")
 pvmonstre1=document.getElementById("viemonstre1");
 texte=document.getElementById("texte");
 imageperso1=document.getElementById("imageperso1");
+let compteurperso = 0;
 
 attaqueperso.onclick=function() {
     pvmonstre1.innerHTML = pvmonstre1.innerHTML-10;
-    texte.innerHTML = "Perso 1 attaque Monstre 1 et inflige 10 de degats !" ;
+    texte.innerHTML = "Perso attaque Monstre 1 et inflige 10 de degats !" ;
     imageperso1.style.left = 10;
     function reposition() {
         imageperso1.style.left = 0;
     }
     setTimeout(reposition,200);
-    function contreattaque() {
-        pvperso1.innerHTML = pvperso1.innerHTML-15;
-        texte.innerHTML = "Monstre 1 contre attaque et inflige 15 de degats !";
+    compteurperso=compteurperso+1;
+    console.log(compteurperso)
+    if (compteurperso == 4) {
+        function contreattaque() {
+            pvperso1.innerHTML = pvperso1.innerHTML-15;
+            texte.innerHTML = "Monstre 1 contre attaque et inflige 15 de degats !";
+        }
+        setTimeout(contreattaque,500);
     }
-    setTimeout(contreattaque,500);
 }
