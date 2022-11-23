797c7a23203c3a83d0e6e7699cafd2c07ebd5524
AshiyroMisachi <riallot.alexandre@outlook.fr>
2022-11-23 17:27:27 +0100

    Système tour et position perso 1 et 2
    lets go je comprends js

--- js.js
+++ js.js
@@ -3,23 +3,49 @@ pvperso1=document.getElementById("vieperso1")
 pvmonstre1=document.getElementById("viemonstre1");
 texte=document.getElementById("texte");
 imageperso1=document.getElementById("imageperso1");
-let compteurperso = 0;
+imageperso2=document.getElementById("imageperso2");
+let compteur = 1;
 
-attaqueperso.onclick=function() {
-    pvmonstre1.innerHTML = pvmonstre1.innerHTML-10;
-    texte.innerHTML = "Perso attaque Monstre 1 et inflige 10 de degats !" ;
-    imageperso1.style.left = 10;
-    function reposition() {
-        imageperso1.style.left = 0;
+
+function positionnement() {
+    if (compteur == 1) {
+        imageperso1.style.left = 20;
     }
-    setTimeout(reposition,200);
-    compteurperso=compteurperso+1;
-    console.log(compteurperso)
-    if (compteurperso == 4) {
-        function contreattaque() {
-            pvperso1.innerHTML = pvperso1.innerHTML-15;
-            texte.innerHTML = "Monstre 1 contre attaque et inflige 15 de degats !";
-        }
-        setTimeout(contreattaque,500);
+    else  if (compteur == 2) {
+        imageperso1.style.left = 0;
+        imageperso2.style.left = 20;
+    }
+    else if (compteur == 3){
+        imageperso1.style.left = 0;
+        imageperso2.style.left = 0;
     }
 }
+
+positionnement()
+
+attaqueperso.onclick = function() {
+    if (compteur == 1){
+        compteur += 1;
+        pvmonstre1.innerHTML = pvmonstre1.innerHTML-10;
+        texte.innerHTML = "Perso 1 attaque Monstre 1 et inflige 10 de degats !" ;
+        imageperso1.style.left = 30;
+        function reposition() {
+            imageperso1.style.left = 20
+        }
+        setTimeout(reposition,200);
+        setTimeout(positionnement,1000);
+    }
+    else if (compteur == 2) {
+        compteur += 1;
+        pvmonstre1.innerHTML = pvmonstre1.innerHTML-10;
+        texte.innerHTML = "Perso 2 attaque Monstre 1 et inflige 10 de degats !";
+        imageperso2.style.left = 30;
+        function reposition2() {
+            imageperso2.style.left = 20;
+        } 
+        setTimeout(reposition2,200);
+        setTimeout(positionnement,1000);
+    }
+}
+
+
